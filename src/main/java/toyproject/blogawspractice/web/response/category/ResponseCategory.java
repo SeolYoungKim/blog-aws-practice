74c3b6a50c61b85a1e86a10a1435b77fc2413387
SeolYoungKim <nasur4da@gmail.com>
2022-08-22 20:57:34 +0900

    Category DTO, User DTO 수정

--- src/main/java/toyproject/blogawspractice/web/response/category/ResponseCategory.java
+++ src/main/java/toyproject/blogawspractice/web/response/category/ResponseCategory.java
@@ -2,20 +2,23 @@ package toyproject.blogawspractice.web.response.category;
 
 import lombok.Getter;
 import toyproject.blogawspractice.domain.category.Category;
-import toyproject.blogawspractice.domain.post.Post;
+import toyproject.blogawspractice.web.response.post.ResponsePost;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ResponseCategory {
 
     private final Long id;
     private final String name;
-    private final List<Post> postList;  //TODO: 순환참조 발생 위험 있음. 아직은 괜찮은듯..?
+    private final List<ResponsePost> postList;
 
     public ResponseCategory(Category category) {
         this.id = category.getId();
         this.name = category.getName();
-        this.postList = category.getPostList();
+        this.postList = category.getPostList().stream()
+                .map(ResponsePost::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/toyproject/blogawspractice/web/response/user/ResponseUser.java
+++ src/main/java/toyproject/blogawspractice/web/response/user/ResponseUser.java
@@ -1,22 +1,25 @@
 package toyproject.blogawspractice.web.response.user;
 
 import lombok.Getter;
-import toyproject.blogawspractice.domain.post.Post;
 import toyproject.blogawspractice.domain.user.User;
+import toyproject.blogawspractice.web.response.post.ResponsePost;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ResponseUser {
     private final String username;
     private final String userEmail;
     private final String userRole;
-    private final List<Post> postList;
+    private final List<ResponsePost> postList;
 
     public ResponseUser(User user) {
         username = user.getUsername();
         userEmail = user.getUserEmail();
         userRole = user.getUserRole().getTitle();
-        postList = user.getPostList();
+        postList = user.getPostList().stream()
+                .map(ResponsePost::new)
+                .collect(Collectors.toList());
     }
 }
